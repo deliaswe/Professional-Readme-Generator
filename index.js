6129b7586970b581f0e0c83eaa490a46de4da5f7
delia <you@example.com>
2023-05-27 18:34:28 -0400

    added code with prompts and questions

--- index.js
+++ index.js
@@ -1,14 +1,66 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const path = require("path");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+.prompt([
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "Please state the same of your project.",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please enter a description of your project.",
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Please enter your installation instructions.",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Please enter usage information.",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Please choose a license for your application.",
+        choices: ["MIT", "APACHE2.0", "Boost", "BSD", "BSD2", "BSD3", "Unlicense", "Open Data Commons", "Mozilla", "IBM", "ISC", "None"],
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "What is the contribution guidelines of your project?",
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Please enter the test instructions.",
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is your GitHub username?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email address.",
+    },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
 function init() {
-    console.log("welcome to readme generator")
+    //console.log("welcome to readme generator")
 }
 
 // Function call to initialize app
